7b382dbcd26d3e8b549d9060bbf4438c10199434
jungu0517 <jungu0517@gmail.com>
2026-02-04 08:59:43 +0000

    feat: Add dark/white mode and new number styling

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 const generateBtn = document.getElementById('generate');
 const numbersDiv = document.getElementById('numbers');
+const themeSwitch = document.getElementById('checkbox');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+themeSwitch.addEventListener('change', switchTheme, false);
 
 generateBtn.addEventListener('click', () => {
     const numbers = new Set();
     while (numbers.size < 6) {
         numbers.add(Math.floor(Math.random() * 45) + 1);
     }
-    numbersDiv.textContent = [...numbers].join(' ');
+    
+    numbersDiv.innerHTML = ''; // Clear previous numbers
+    
+    for (const number of [...numbers].sort((a, b) => a - b)) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('number');
+        numberDiv.textContent = number;
+        numbersDiv.appendChild(numberDiv);
+    }
 });
